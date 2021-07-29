bb8b31d257efdd62d791faeb3a1b5af94eaf4fb2
Adina <adrianna.manea@gmail.com>
2021-07-29 12:00:51 +0200

    14_indentation_twick

--- 14_Animated_Navigation/script.js
+++ 14_Animated_Navigation/script.js
@@ -1,6 +1,4 @@
 const nav = document.getElementById('nav');
 const toggle = document.getElementById('toggle');
 
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
+toggle.addEventListener('click', () => nav.classList.toggle('active'));
