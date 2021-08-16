a1d6429ab8d621104904b2e327d908b78c663373
Adina <adrianna.manea@gmail.com>
2021-08-16 19:15:40 +0200

    19_js

--- 19_Clock/script.js
+++ 19_Clock/script.js
@@ -26,3 +26,46 @@ toggleEl.addEventListener('click', (e) => {
     e.target.innerHTML = 'Light mode';
   }
 });
+
+function setTime() {
+  const time = new Date();
+  // console.log(time);
+  const month = time.getMonth();
+  // console.log(month);
+  const day = time.getDay();
+  // console.log(day);
+  const date = time.getDate();
+  // console.log(date);
+  const hours = time.getHours();
+  // console.log(hours);
+  const minutes = time.getMinutes();
+  // console.log(minutes);
+  const hoursForClock = hours % 12 + minutes / 60;
+  // console.log(hoursForClock);
+  const seconds = time.getSeconds();
+  // console.log(seconds);
+
+  hourEl.style.transform = `translate(-50%, -100%) rotate(${hoursForClock/ 12 * 360}deg)`;
+  minuteEl.style.transform = `translate(-50%, -100%) rotate(${minutes/ 60 * 360}deg)`;
+  secondEl.style.transform = `translate(-50%, -100%) rotate(${seconds/ 60 * 360}deg)`;
+
+
+  // const hoursForClock = hours % 12;
+  // const ampm = hours >= 12 ? 'PM' : 'AM';
+  // hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`;
+  // minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`;
+  // secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`;
+  // timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`;
+
+  timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}`;
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`;
+}
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+// const scale = (number, inMin, inMax, outMin, outMax) => {
+//   return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+// }
+
+setTime();
+
+setInterval(setTime, 1000);
