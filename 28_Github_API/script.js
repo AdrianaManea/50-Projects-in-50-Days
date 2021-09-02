0de19f19d2344648880705e37ec9f09cdd62a04c
Adina <adrianna.manea@gmail.com>
2021-09-02 16:38:00 +0200

    28_first commit

--- 28_Github_API/script.js
+++ 28_Github_API/script.js
@@ -2,6 +2,9 @@ const APIURL = 'https://api.github.com/users/';
 
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const main = document.getElementById('main');
+
+
 // getUser('adrianamanea');
 
 // function getUser(username) {
@@ -20,10 +23,49 @@ async function getUser(username) {
     const {
       data
     } = await axios.get(APIURL + username);
-    console.log(data);
+    // console.log(data);
+    createUserCard(data);
   } catch (error) {
-    console.log(error);
+    // console.log(error);
+    if (error.response.status == 404) {
+      createErrorCard('No profile with this username');
+    }
   }
+}
+
+
+function createUserCard(user) {
+  const cardHTML = `
+  <div class="card">
+    <div>
+      <img src="${user.avatar_url}" alt="${user.name}" class="avatar">
+    </div>
+    <div class="user-info">
+      <h2>${user.name}</h2>
+      <p>${user.bio == null ? "Bio is not available for this user" : user.bio}</p>
+
+      <ul>
+        <li>${user.followers} <strong>Followers</strong></li>
+        <li>${user.following} <strong>Following</strong></li>
+        <li>${user.public_repos} <strong>Repos</strong></li>
+      </ul>
+
+      <div id="repos"></div>
+    </div>
+  </div>
+  `;
+
+  main.innerHTML = cardHTML;
+}
+
+function createErrorCard(message) {
+  const cardHTML = `
+    <div class="card">
+      <h1>${message}</h1>
+    </div>
+  `;
+
+  main.innerHTML = cardHTML;
 }
 
 form.addEventListener('submit', (e) => {
@@ -36,4 +78,4 @@ form.addEventListener('submit', (e) => {
 
     search.value = '';
   }
-})
+});
